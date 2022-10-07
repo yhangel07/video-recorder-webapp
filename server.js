fc0e7a2d1c1fc2be4c90754a8fca856525676067
Arianne Coyco <yhangel.07@gmail.com>
2022-10-07 23:25:26 +0800

    changing build path

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist/kembaq-web')));
+app.use(express.static(path.join(__dirname, '/dist/video-recorder-webapp')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/kembaq-web/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/video-recorder-webapp/index.html'));
 });
 
 app.listen(process.env.PORT || 4200, () => {
